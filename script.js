a5cb025b1bcf8fdf1131c3cd13e9b4f7718dc14c
WannyMartins <wcatery@gmail.com>
2021-12-11 14:47:57 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
-var elementOl = document.getElementById("#lista-tarefas");
-var elementoLi;
-var inputName= document.getElementById("texto-tarefa");
-var botao = document.getElementById('criar-tarefa'); 
- 
+var botao = document.getElementById('criar-tarefa');
 
-
-botao.addEventListener('click', function (){
-    let criaLi=document.createElement('li');
-    let listaOrdenada = document.querySelector("#lista-tarefas");
-    var value = inputName.value;
-    criaLi.innerHTML= value;
-    listaOrdenada.appendChild(criaLi);
-    console.log(value)
-    
-    
-    }  )
-    
-
-
+function criarTarefa() {
+    botao.addEventListener('click', itemLi);
   
+    function itemLi() {
+      var li = document.createElement('li');
+      var input = document.getElementById('texto-tarefa').value;//aprendi sobre input.value para armazenar o valor digitado no input....neste video ...https://www.youtube.com/watch?v=bC5Mp37L5hA&ab_channel=MatheusBattisti-HoradeCodar
+
+      li.innerText = input;
+      document.getElementById('lista-tarefas').appendChild(li);
+      document.getElementById('texto-tarefa').value = '';//aqui indforma que input volta a ficar vazio após criar a lista com o valor do input.
+    }
+  }
+  criarTarefa();
