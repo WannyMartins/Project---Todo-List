68d680ce40825f5c229288c47a0c71c06ec4b156
Wanny Martins <wcatery@gmail.com>
2022-01-23 16:17:27 -0300

    corrigindo lint

--- script.js
+++ script.js
@@ -1,89 +1,87 @@
+// Variáveis
 
-var botao = document.getElementById('criar-tarefa');
-var listaTarefas = document.getElementById('lista-tarefas')
-
-const tarefaI= JSON.parse(localStorage.getItem('tarefas')) ||
-[];
-
-
-function criarTarefa() {
-    botao.addEventListener('click', itemLi);
-  
-    function itemLi() {
-      const li = document.createElement('li');
-      var input = document.getElementById('texto-tarefa').value;//aprendi sobre input.value para armazenar o valor digitado no input....neste video ...https://www.youtube.com/watch?v=bC5Mp37L5hA&ab_channel=MatheusBattisti-HoradeCodar
-
-      li.innerText = input;
-      li.className = 'item-list'
-      tarefaI.push({
-        listaT : input
-       
- 
-     } )
- 
-   
-     
-      document.getElementById('lista-tarefas').appendChild(li);
-
-       document.getElementById('texto-tarefa').value = '';//aqui indforma que input volta a ficar vazio após criar a lista com o valor do input.
-
-    } 
-}
-criarTarefa();
-
-listaTarefas.addEventListener('click', function(event){
-    let listaItemSelecionado = document.querySelectorAll('.selected');
-    let selecionaItem = document.querySelector('.selected');
-
-    if (listaItemSelecionado.length>0){
-        selecionaItem.classList.remove('selected');
-        selecionaItem = event.target;
-        event.target.classList.add('selected');
-    } else {
-        event.target.classList.add('selected');
-    }
-})
-
-listaTarefas.addEventListener('dblclick', function(event){  //referencia ao dblclick >>>>https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
-    let selecionaItem = document.querySelector('.completed');
-
-        if(selecionaItem!=true){
-            selecionaItem = event.target;
-            event.target.classList.toggle('completed');//toggle tirei referencia no mdn >>>> https://developer.mozilla.org/en-US/search?q=toggle
-        }
-    
-})
-
-let botaoApagaTudo = document.querySelector("#apaga-tudo");
+const botao = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const tarefaI = [];
+let selecionaItem = document.querySelector('.completed');
+const botaoApagaTudo = document.querySelector('#apaga-tudo');
+const botaoApagaCompleted = document.querySelector('#remover-finalizados');
+const botaoSalvarLista = document.getElementById('salvar-tarefas');
+const tarefaSalva = JSON.parse(localStorage.getItem('tarefas'));
+const tarefasSalvas = [];
 
 
-botaoApagaTudo.addEventListener('click', function (event){
+// functions
+const itemLi = () => {
+  const li = document.createElement('li');
+  const input = document.getElementById('texto-tarefa').value; // aprendi sobre input.value para armazenar o valor digitado no input....neste video ...https://www.youtube.com/watch?v=bC5Mp37L5hA&ab_channel=MatheusBattisti-HoradeCodar
 
-    while(listaTarefas.firstChild){
-        listaTarefas.removeChild(listaTarefas.firstChild)
-    }//https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
-    //while funciona como for, no caso enquanto lista de tarefas tiver primeiro filho ele vai executar a remoção deste primeiro filho. não existindo mais o primeiro o segundo passa a ser primeiro e ai é removido tambem.
-    tarefaI.splice(0,tarefaI.length )
-    localStorage.clear('tarefas');
-})
+  li.innerText = input;
+  li.className = 'item-list';
+  tarefaI.push({
+    listaT: input,
+  });
 
-let botaoApagaCompleted = document.querySelector('#remover-finalizados');
+  listaTarefas.appendChild(li);
 
+  document.getElementById('texto-tarefa').value = ''; // aqui indforma que input volta a ficar vazio após criar a lista com o valor do input.
+};
 
-botaoApagaCompleted.addEventListener('click', function (event){
+const selecionado = (event) => {
+  const listaItemSelecionado = document.querySelectorAll('.selected');
+  let selecionaItem = document.querySelector('.selected');
 
-    while(document.querySelector('.completed')){
-        let remove = listaTarefas.removeChild(listaTarefas.querySelector('.completed'))
+  if (listaItemSelecionado.length > 0) {
+    selecionaItem.classList.remove('selected');
+    selecionaItem = event.target;
+    event.target.classList.add('selected');
+  } else {
+    event.target.classList.add('selected');
+  }
+};
 
-    }
-
-})
-const botaoSalvarLista=document.getElementById('salvar-tarefas');
-botaoSalvarLista.addEventListener('click', salvar)
-
+const completed = (event) => {
+  if (selecionaItem !== true) {
+    selecionaItem = event.target;
+    event.target.classList.toggle('completed'); //toggle tirei referencia no mdn >>>> https://developer.mozilla.org/en-US/search?q=toggle
+  }
+};
+const apagarLista = () => {
+  while (listaTarefas.firstChild) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  } // https://developer.mozilla.org/pt-BR/docs/Web/API/Node/removeChild
+  // while é um for, no caso enquanto lista de tarefas tiver primeiro filho ele vai executar a remoção deste primeiro filho. não existindo mais o primeiro o segundo passa a ser primeiro e ai é removido tambem.
+  tarefaI.splice(0, tarefaI.length);
+  localStorage.clear('tarefas');
+};
+const removerCompleto = () => {
+  while (selecionaItem) {
+    listaTarefas.removeChild(selecionaItem);
+  }
+};
 // tive ajuda do colega Leo Araujo para desenrolar o localStorage
-function salvar(){
-localStorage.setItem('tarefas', JSON.stringify(tarefaI))
-}
-   
+const salvar = () => localStorage.setItem('tarefas', JSON.stringify(tarefaI));
+const recuperarLista = () => {
+  for (let index = 0; index < tarefaSalva.length; index += 1) {
+    const li = document.createElement('li');
+    li.innerText = tarefaSalva[index].listaT;
+    li.className = 'item-list';
+    tarefasSalvas.push(tarefaSalva[index]);
+    listaTarefas.appendChild(li);
+  }
+};
 
+// Eventos
+
+botao.addEventListener('click', itemLi);
+listaTarefas.addEventListener('click', selecionado);
+listaTarefas.addEventListener('dblclick', completed); // referencia ao dblclick >>>> https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+botaoApagaCompleted.addEventListener('click', removerCompleto);
+botaoApagaTudo.addEventListener('click', apagarLista);
+botaoSalvarLista.addEventListener('click', salvar);
+
+window.onload = function atualizar() {
+  if (localStorage.length > 0) {
+    recuperarLista();
+  }
+};
